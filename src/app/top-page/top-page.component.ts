60c43f8ed4488848e5ba0d53b7c1279b36d04e4f
beyanon <tokabe333@gmail.com>
2021-01-06 02:36:25 +0900

    Update top-page.component.ts

--- src/app/top-page/top-page.component.ts
+++ src/app/top-page/top-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeaderService } from '../header/header.service';
+
 @Component({
-  selector: 'app-top-page',
-  templateUrl: './top-page.component.html',
-  styleUrls: ['./top-page.component.scss']
+	selector: 'app-top-page',
+	templateUrl: './top-page.component.html',
+	styleUrls: ['./top-page.component.scss']
 })
 export class TopPageComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private headerService: HeaderService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.headerService.setMidasi("top-page");
+	}
 
 }
