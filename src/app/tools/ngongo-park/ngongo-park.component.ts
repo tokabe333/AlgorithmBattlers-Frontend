7273e29e73503b8ae305888463a3e06f5101604d
beyanon <tokabe333@gmail.com>
2021-01-05 22:50:53 +0900

    update : 見出し作る

--- src/app/tools/ngongo-park/ngongo-park.component.ts
+++ src/app/tools/ngongo-park/ngongo-park.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-ngongo-park',
-  templateUrl: './ngongo-park.component.html',
-  styleUrls: ['./ngongo-park.component.scss']
+	selector: 'app-ngongo-park',
+	templateUrl: './ngongo-park.component.html',
+	styleUrls: ['./ngongo-park.component.scss']
 })
 export class NgongoParkComponent implements OnInit {
+	@Input() ngongotext!: string;
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
