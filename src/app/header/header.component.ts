80ef444e36eac928ad5565e082763aca9c51fbe2
beyanon <tokabe333@gmail.com>
2021-01-05 23:07:03 +0900

    update : 見出しをページごとに変更

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-	midasi: string = "Welcome to NgoNgoPark";
-	constructor() { }
+	public nowMidasi: string = "Welcome to NgoNgoPark";
+
+	private midasi: { [key: string]: string; } = {
+		"apis": "Utility APIs over view!",
+		"submit": "Submit Page Here!",
+		"ranking": "Tsuyo Tsuyo Ojisan Ranking!",
+		"top-page": "Welcome to NgoNgoPark!",
+	};
+
+	constructor(private router: Router) { }
 
 	ngOnInit(): void {
+	}
+
+	linksOnclick(page: string): void {
+		this.nowMidasi = this.midasi[page];
+		this.router.navigate([page]);
 	}
 
 }
