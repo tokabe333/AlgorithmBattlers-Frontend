804dea9da15d41a448deba3a178969cc4af9349a
beyanon <tokabe333@gmail.com>
2021-01-05 23:33:07 +0900

    update:  JSON形式追加

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ranking',
-  templateUrl: './ranking.component.html',
-  styleUrls: ['./ranking.component.scss']
+	selector: 'app-ranking',
+	templateUrl: './ranking.component.html',
+	styleUrls: ['./ranking.component.scss']
 })
 export class RankingComponent implements OnInit {
+	private url: string = "http://160.251.20.191/HoshideCompany/AIbattle/ranking";
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
