1495b97e555af13ba88d0382a8a6f14f86c289d9
beyanon <tokabe333@gmail.com>
2021-01-05 17:56:31 +0900

    update : 各ページのコンポーネント追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RankingComponent } from './ranking/ranking.component';
+import { SubmitComponent } from './submit/submit.component';
+import { TopPageComponent } from './top-page/top-page.component';
+
+const routes: Routes = [
+	{ path: "", redirectTo: "/top-page", pathMatch: "full" },
+	{ path: "top-page", component: TopPageComponent },
+	{ path: "submit", component: SubmitComponent },
+	{ path: "ranking", component: RankingComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SubmitComponent } from './submit/submit.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { TopPageComponent } from './top-page/top-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		SubmitComponent,
+		RankingComponent,
+		TopPageComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
