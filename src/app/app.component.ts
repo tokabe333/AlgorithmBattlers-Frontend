3a3c7538b1aacca301407ff6d1671f6e0eda0be4
beyanon <tokabe333@gmail.com>
2021-01-05 17:39:36 +0900

    update : headerに苦戦中

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AlgorithmBattlers-Frontend';
+	title = 'AlgorithmBattlers-Frontend';
 }
