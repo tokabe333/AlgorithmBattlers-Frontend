73d5d9244f481fb1d53f07fa43fe2615eb90b213
beyanon <tokabe333@gmail.com>
2021-01-07 00:03:32 +0900

    update : 自前でpretty呼べば大丈夫そう

--- src/app/apis/apis.component.ts
+++ src/app/apis/apis.component.ts
@@ -1,16 +1,17 @@
-import { Component, ComponentFactory, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef, AfterViewInit, ComponentRef } from '@angular/core';
+import { Component, ComponentFactory, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef, AfterViewInit, AfterViewChecked, ComponentRef } from '@angular/core';
 
 import { HeaderService } from '../header/header.service';
 import { PrettyprintComponentComponent } from '../tools/prettyprint-component/prettyprint-component.component';
 import { H2ComponentComponent } from '../tools/h2-component/h2-component.component';
 import { IAPIs, APICodes, LangList } from './api_codes';
+import { $ } from 'protractor';
 
 @Component({
 	selector: 'app-apis',
 	templateUrl: './apis.component.html',
 	styleUrls: ['./apis.component.scss']
 })
-export class APIsComponent implements OnInit, AfterViewInit {
+export class APIsComponent implements OnInit, AfterViewInit, AfterViewChecked {
 	public apiCodes!: { [key: string]: IAPIs };
 	public langList!: string[];
 
@@ -21,6 +22,11 @@ export class APIsComponent implements OnInit, AfterViewInit {
 	@ViewChild("codes", { read: ViewContainerRef }) viewContainerRef!: ViewContainerRef;
 
 	h2_1!: ComponentRef<H2ComponentComponent>;
+	h2_2!: ComponentRef<H2ComponentComponent>;
+	code_1!: ComponentRef<PrettyprintComponentComponent>;
+	code_2!: ComponentRef<PrettyprintComponentComponent>;
+
+	declare PR: any;
 
 	constructor(
 		private headerService: HeaderService,
@@ -35,21 +41,51 @@ export class APIsComponent implements OnInit, AfterViewInit {
 
 		this.prettyPrintComponent = this.resolver.resolveComponentFactory(PrettyprintComponentComponent);
 		this.h2Component = this.resolver.resolveComponentFactory(H2ComponentComponent);
-
-	}
-
-	onChange(): void {
-		this.h2_1 = this.viewContainerRef.createComponent(this.h2Component);
-		setTimeout(() => { this.h2_1.instance.h2text = "Board Data Getter!"; });
-
-
-		const code_1 = this.viewContainerRef.createComponent(this.prettyPrintComponent);
-		code_1.instance.lang = this.dropdownSelected;
-		code_1.instance.apiList = 1;
-
+		this.PR = require("../../prettify.js");
 	}
 
 	ngAfterViewInit() {
-		this.onChange();
+		this.generate();
 	}
+
+	ngAfterViewChecked() {
+		this.applyPretty();
+	}
+
+	onChange(): void {
+		this.generate();
+	}
+
+	applyPretty(): void {
+		this.PR.prettyPrint();
+		//console.log(this.PR.prettyPrint);
+	}
+
+
+	generate(): Promise<void> {
+		if (this.h2_1 != undefined) { this.h2_1.destroy(); }
+		if (this.h2_2 != undefined) { this.h2_2.destroy(); }
+		if (this.code_1 != undefined) { this.code_1.destroy(); }
+		if (this.code_2 != undefined) { this.code_2.destroy(); }
+
+		return new Promise(() => {
+			setTimeout(() => {
+				this.h2_1 = this.viewContainerRef.createComponent(this.h2Component);
+				this.h2_1.instance.h2text = "Board Data Getter!";
+
+				this.code_1 = this.viewContainerRef.createComponent(this.prettyPrintComponent);
+				this.code_1.instance.lang = this.dropdownSelected;
+				this.code_1.instance.apiList = 1;
+
+				this.h2_2 = this.viewContainerRef.createComponent(this.h2Component);
+				this.h2_2.instance.h2text = "Board Data Poster!";
+
+				this.code_2 = this.viewContainerRef.createComponent(this.prettyPrintComponent);
+				this.code_2.instance.lang = this.dropdownSelected;
+				this.code_2.instance.apiList = 2;
+			});
+		});
+	}
+
+
 }
