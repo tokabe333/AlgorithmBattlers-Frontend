09289f182b1b2a4237a3fea70f4ff78c3dc8cea1
beyanon <tokabe333@gmail.com>
2021-01-10 19:39:51 +0900

    update : Submit先変更

--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -20,7 +20,7 @@ export class SubmitComponent implements OnInit {
 
 
 	//private url: string = "http://127.0.0.1:8888/users";
-	private url: string = "http://160.251.20.191/api/submit";
+	private url: string = "http://160.251.20.191/HoshideCompany/AIbattle/api/submit";
 	declare PR: any;
 
 	constructor(
